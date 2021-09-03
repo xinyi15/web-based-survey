e282b06b898efd1f9e7452e438300fa419aaa41b
Mincen Liu <mincen@live.unc.edu>
2021-09-03 15:30:31 -0400

    update

---
+++ build/script.js
@@ -0,0 +1,51 @@
+import surveylist from "/surveylist.js";
+
+window.survey = new Survey.Model(surveylist);
+
+var myCss = {
+    matrix: {root: "table table-striped matrixtable"},
+};
+
+$("#surveyContainer").Survey({
+    model: survey,
+    css: myCss
+});
+
+survey
+    .onComplete
+    .add(function (sender) {
+        document
+            .querySelector('#surveyResult')
+            .textContent = "Result JSON:\n" + JSON.stringify(sender.data, null, 3);
+    });
+
+    function sendDataToTheServer(isComplete, data) {
+        var text = isComplete ? "The survey is completed" : "The survey is not completed";
+        document.querySelector('#surveyResults').innerHTML = text + ", result: " + JSON.stringify(data);
+    }
+    
+    survey.onComplete.add(function(survey, options) {
+        sendDataToTheServer(true, survey.data);
+    });
+    
+    survey.onPartialSend.add(function(survey, options) {
+        sendDataToTheServer(false, survey.data);
+    });
+
+var defaultThemeColors = Survey
+    .StylesManager
+    .ThemeColors["default"];
+defaultThemeColors["$main-color"] = "#4B9CD3";
+defaultThemeColors["$main-hover-color"] = "#4B9CD3";
+defaultThemeColors["$text-color"] = "#4a4a4a";
+defaultThemeColors["$header-color"] = "#4B9CD3";
+
+defaultThemeColors["$header-background-color"] = "#4a4a4a";
+defaultThemeColors["$body-container-background-color"] = "#f8f8f8";
+
+Survey
+    .StylesManager
+    .applyTheme();
+
+$(".sv_prev_btn").remove(); // hide the previous button
+
