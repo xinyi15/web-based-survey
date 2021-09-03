638c2d50e2ba760e11c446b87e3c164f8b9089c1
Mincen Liu <mincen@live.unc.edu>
2021-09-03 15:18:58 -0400

    Sept 3

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+
+
+const app = express()
+//app.set("view options", {layout: false});
+// app.use(express.static(path.join(__dirname, 'build'))); 
+app.post('/', function(req, res) { res.sendFile(path.join(__dirname + '/index.html')); });
+
+
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
