3b33a23e7506a86fcd7b0d7fcbab1c01449bed32
Mincen Liu <mincen@live.unc.edu>
2021-11-04 15:07:26 -0400

    check data

--- build/script.js
+++ build/script.js
@@ -16,19 +16,32 @@ survey
     .add(function (sender) {
         document
             .querySelector('#surveyResult')
-            .textContent = "Result JSON:\n dazhuxiaozhu" + JSON.stringify(sender.data, null, 3);
+            .textContent = "Result JSON:\n" + JSON.stringify(sender.data, null, 3);
     });
 
     function sendDataToTheServer(isComplete, data) {
+        var timeNow = new Date();
         var text = isComplete ? "The survey is completed" : "The survey is not completed";
-        document.querySelector('#surveyResults').innerHTML = text + ", result: " + JSON.stringify(data);
+        document.querySelector('#surveyResults').innerHTML = text + ", result: " + JSON.stringify(data[0]);
+        // const { Pool } = require('pg')
+        // const pool = new Pool({
+        // user: 'jovttytttjkoaw',
+        // host: 'ec2-52-23-45-36.compute-1.amazonaws.com',
+        // database: 'de369o7l6fqn9p',
+        // password: '77d7812505f2328350b04d9fb5bb45ade034c24a216d4ac59682c55fcce5941b',
+        // port: 5432,
+        // })
+        // pool.query('INSERT INTO answer (id, Time, survey, question, answer, date) VALUES (999,'+ timeNow +','+"'Morning',"+data[0]+"," +data[data[0]]+","+"10)", (err, res) => {
+        // console.log(err, res);
+        // pool.end();
+        // });
     }
     
-    survey.onComplete.add(function(survey, options) {
+    survey.onComplete.add(function(survey) {
         sendDataToTheServer(true, survey.data);
     });
     
-    survey.onPartialSend.add(function(survey, options) {
+    survey.onPartialSend.add(function(survey) {
         sendDataToTheServer(false, survey.data);
     });
 
@@ -48,20 +61,3 @@ Survey
     .applyTheme();
 
 $(".sv_prev_btn").remove(); // hide the previous button
-
-const { Pool } = require('pg')
-const pool = new Pool({
-  user: 'jovttytttjkoaw',
-  host: 'ec2-52-23-45-36.compute-1.amazonaws.com',
-  database: 'de369o7l6fqn9p',
-  password: '77d7812505f2328350b04d9fb5bb45ade034c24a216d4ac59682c55fcce5941b',
-  port: 5432,
-})
-pool.query('SELECT NOW()', (err, res) => {
-  console.log(err, res)
-  pool.end()
-})
-
-// sql insert table value
-// INSERT INTO answer
-//  ()
