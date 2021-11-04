ce258ced6a09f3efed7a2ab22f4e8ac94f99571f
Mincen Liu <mincen@live.unc.edu>
2021-11-04 14:28:29 -0400

    send answer

--- build/script.js
+++ build/script.js
@@ -16,7 +16,7 @@ survey
     .add(function (sender) {
         document
             .querySelector('#surveyResult')
-            .textContent = "Result JSON:\n" + JSON.stringify(sender.data, null, 3);
+            .textContent = "Result JSON:\n dazhuxiaozhu" + JSON.stringify(sender.data, null, 3);
     });
 
     function sendDataToTheServer(isComplete, data) {
@@ -49,3 +49,19 @@ Survey
 
 $(".sv_prev_btn").remove(); // hide the previous button
 
+const { Pool } = require('pg')
+const pool = new Pool({
+  user: 'jovttytttjkoaw',
+  host: 'ec2-52-23-45-36.compute-1.amazonaws.com',
+  database: 'de369o7l6fqn9p',
+  password: '77d7812505f2328350b04d9fb5bb45ade034c24a216d4ac59682c55fcce5941b',
+  port: 5432,
+})
+pool.query('SELECT NOW()', (err, res) => {
+  console.log(err, res)
+  pool.end()
+})
+
+// sql insert table value
+INSERT INTO answer
+ ()
