d83031cf52832b011fa22f811e014cf85f95ee5b
Mincen Liu <mincen@live.unc.edu>
2021-11-10 15:29:15 -0500

    test15

--- index.js
+++ index.js
@@ -4,15 +4,21 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 const { Pool } = require('pg');
 
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
-
-var pool = new Pool({
-    user: 'jovttytttjkoaw',
-    host: 'ec2-52-23-45-36.compute-1.amazonaws.com',
-    database: 'de369o7l6fqn9p',
-    password: '77d7812505f2328350b04d9fb5bb45ade034c24a216d4ac59682c55fcce5941b',
-    port: 5432,
-    });
+// var pool = new Pool({
+//     user: 'jovttytttjkoaw',
+//     host: 'ec2-52-23-45-36.compute-1.amazonaws.com',
+//     database: 'de369o7l6fqn9p',
+//     password: '77d7812505f2328350b04d9fb5bb45ade034c24a216d4ac59682c55fcce5941b',
+//     port: 5432,
+//     });
 var timeNow = new Date();
 const app = express()
 //app.set("view options", {layout: false});
