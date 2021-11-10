bcf6d8aaa0cfe8cbaaf854be1a4b273185b4ce6b
Mincen Liu <mincen@live.unc.edu>
2021-11-10 17:10:07 -0500

    T41

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.post('/answer',async (req,res)=>{
   console.log('INSERT INTO answer (id, time, survey, question, answer, date) VALUES ("'+ req.body.id+'","'+ req.body.time+'","'+req.body.survey+'","'+req.body.question +'","'+req.body.answer+'","'+req.body.date+'")');
 const client = await pool.connect();
-var result = await client.query('INSERT INTO answer (id, time, survey, question, answer, date) VALUES ("'+ req.body.id+'","'+ req.body.time+'","'+req.body.survey+'","'+req.body.question +'","'+req.body.answer+'","'+req.body.date+'")'
+var result = await client.query('INSERT INTO answer (id, time, survey, question, answer, date) VALUES ('+ req.body.id+',"'+ req.body.time+'","'+req.body.survey+'","'+req.body.question +'","'+req.body.answer+'","'+req.body.date+'")'
 );
  client.release();
  res.send('WWWWWÍ');
