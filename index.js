4395a886e20f4485725b8d49015687f4c4ef5f17
Mincen Liu <mincen@live.unc.edu>
2021-11-10 15:32:00 -0500

    test20

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const { read } = require('fs');
 const path = require('path')
 const PORT = process.env.PORT || 5000
 const { Pool } = require('pg');
-
-const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
