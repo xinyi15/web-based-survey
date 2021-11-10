725f236865bdf5c34e7e1fd2598a94368582c560
Mincen Liu <mincen@live.unc.edu>
2021-11-10 13:38:22 -0500

    test7

--- index.js
+++ index.js
@@ -18,11 +18,11 @@ const app = express()
 //app.set("view options", {layout: false});
 app.use(express.static(path.join(__dirname, 'build'))); 
 app.post('/answer',function(req,res){
-    console.log("asssss");
+    console.log(req.body.id);
     //pool.query('INSERT INTO answer (id, Time, survey, question, answer, date) VALUES ("'+ req.body.id+'","'+ req.body.time+'","'+req.body.survey+'","'+req.body.question +'","'+req.body.answer+'",'+"10)", (err, res) => {
        // console.log(err, res);
      //   pool.end();
-        res.send('WWWWWÍ');
+      //  res.send('WWWWWÍ');
         // });
 })
 app.post('/', function(req, res) { res.sendFile(path.join(__dirname + '/build/index.html')); });
