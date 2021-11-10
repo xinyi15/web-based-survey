377d4577aa9ab4c0c702a9daf6dbe0bf826ad0b5
Mincen Liu <mincen@live.unc.edu>
2021-11-10 12:45:25 -0500

    fetch

--- build/script.js
+++ build/script.js
@@ -1,4 +1,6 @@
+import { response } from "express";
 import surveylist from "/surveylist.js";
+import fetch from 'node-fetch';
 
 window.survey = new Survey.Model(surveylist);
 
@@ -20,22 +22,26 @@ survey
     });
 
     function sendDataToTheServer(isComplete, data) {
-        var timeNow = new Date();
         var text = isComplete ? "The survey is completed" : "The survey is not completed";
         var a=Object.keys(data);
         document.querySelector('#surveyResults').innerHTML = text + ", result: " + JSON.stringify(data);
-        const { Pool } = require('pg');
-        const pool = new Pool({
-        user: 'jovttytttjkoaw',
-        host: 'ec2-52-23-45-36.compute-1.amazonaws.com',
-        database: 'de369o7l6fqn9p',
-        password: '77d7812505f2328350b04d9fb5bb45ade034c24a216d4ac59682c55fcce5941b',
-        port: 5432,
+        var url="https://web-based-survey.herokuapp.com/answer";
+        var timeNow = new Date();
+        const response=fetch(url,{
+            method:'POST',
+            body: JSON.stringify({
+                id:1,
+                Time: timeNow, 
+                survey: "Morning", 
+                question: a[a.length-1], 
+                answer: data[a[a.length-1]], 
+                date: 10
+            }),
+            headers:{'Content-Type':'application/json'}
         });
-        pool.query('INSERT INTO answer (id, Time, survey, question, answer, date) VALUES (999,'+ timeNow +','+"'Morning',"+a[a.length-1]+"," +data[a[a.length-1]]+","+"10)", (err, res) => {
-        console.log(err, res);
-        pool.end();
-        });
+        const data=await response.json();
+        console.log(data);
+   
     }
 
     // const { Pool } = require('pg');
--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const { Pool } = require('pg');
 
 
+const pool = new Pool({
+user: 'jovttytttjkoaw',
+host: 'ec2-52-23-45-36.compute-1.amazonaws.com',
+database: 'de369o7l6fqn9p',
+password: '77d7812505f2328350b04d9fb5bb45ade034c24a216d4ac59682c55fcce5941b',
+port: 5432,
+});
 
+var timeNow = new Date();
 const app = express()
 //app.set("view options", {layout: false});
 app.use(express.static(path.join(__dirname, 'build'))); 
+app.get('/answer',function(req,res){
+    console.log("asssss",req.body.id);
+    pool.query('INSERT INTO answer (id, Time, survey, question, answer, date) VALUES ('+ req.body.id+','+ req.body.time+','+req.body.survey+','+req.body.question +","+req.body.answer+","+"10)", (err, res) => {
+        console.log(err, res);
+        pool.end();
+        });
+})
 app.post('/', function(req, res) { res.sendFile(path.join(__dirname + '/build/index.html')); });
 
 
