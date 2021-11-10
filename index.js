c7e82bbadfd51cc9acfdb9e056f41c3fc77cae77
Mincen Liu <mincen@live.unc.edu>
2021-11-10 14:47:33 -0500

    test11

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ var timeNow = new Date();
 const app = express()
 //app.set("view options", {layout: false});
 app.use(express.static(path.join(__dirname, 'build'))); 
-app.post('/answer',function(req,res){
+app.get('/answer',function(req,res){
     // console.log(req.body);
     //pool.query('INSERT INTO answer (id, Time, survey, question, answer, date) VALUES ("'+ req.body.id+'","'+ req.body.time+'","'+req.body.survey+'","'+req.body.question +'","'+req.body.answer+'",'+"10)", (err, res) => {
        // console.log(err, res);
