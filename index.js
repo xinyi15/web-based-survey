260fd9059b63e54a959b55a437038cef93300638
Zhang Xinyi <surveyappunc@gmail.com>
2022-02-07 14:09:26 -0500

    using git for heroku deployment

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ res.send('Success');
 
   app.get('/route'+"1", function(req, res){
   res.sendFile(path.join(__dirname + '/build/index.html')); 
-  res.redirect('/route1?id=1');
+  res.set({'id':1});
 });
 
 
