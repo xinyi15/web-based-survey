d2aa714e76cf2fc3bf6009467d4cb03a1fa61945
Zhang Xinyi <surveyappunc@gmail.com>
2022-02-07 11:56:36 -0500

    using git for heroku deployment

--- twilio.js
+++ twilio.js
@@ -2,16 +2,17 @@ require('dotenv').config();
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
-
+const p =process.env.TWILIO_PHONE_NUMBER;
 const sendSms = (phone, message) => {
 
   const client_ = require('twilio')(accountSid, authToken);
-  console.log(process.env.TWILIO_PHONE_NUMBER);
+  console.log(p);
   client_.messages
     .create({
        body: message,
        //messagingServiceSid: 'MG72517cc4359f2b3e5d53361655d9721d', 
-       from: process.env.TWILIO_PHONE_NUMBER,
+       //from: process.env.TWILIO_PHONE_NUMBER,
+       from: p,
        to: phone
      })
     .then(message => console.log(message.sid));
