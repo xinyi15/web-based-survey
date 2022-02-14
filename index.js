e91090fa5a7f81e6f2208a199393717b773c793e
Zhang Xinyi <surveyappunc@gmail.com>
2022-02-14 14:11:21 -0500

    using git for heroku deployment

--- index.js
+++ index.js
@@ -29,20 +29,20 @@ app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => res.render('pages/survey'))
 
- app.post('/answer',async (req,res)=>{
- console.log('INSERT INTO answer (id, time, survey, question, answer, date) VALUES ("'+ req.body.id+'","'+ req.body.time+'","'+req.body.survey+'","'+req.body.question +'","'+req.body.answer+'","'+req.body.date+'")');
-const client = await pool.connect();
- var result = await client.query("INSERT INTO answer (id, time, survey, question, answer, date) VALUES ("+ req.body.id+",'"+ req.body.time+"','"+req.body.survey+"','"+req.body.question +"','"+req.body.answer+"','"+req.body.date+"')"
- );
- client.release();
+//  app.post('/answer',async (req,res)=>{
+//  console.log('INSERT INTO answer (id, time, survey, question, answer, date) VALUES ("'+ req.body.id+'","'+ req.body.time+'","'+req.body.survey+'","'+req.body.question +'","'+req.body.answer+'","'+req.body.date+'")');
+// const client = await pool.connect();
+//  var result = await client.query("INSERT INTO answer (id, time, survey, question, answer, date) VALUES ("+ req.body.id+",'"+ req.body.time+"','"+req.body.survey+"','"+req.body.question +"','"+req.body.answer+"','"+req.body.date+"')"
+//  );
+//  client.release();
 
- const Message = 'Please complete your survey';
- console.log(phone_num["1"]);
- sendSms(phone_num[1], Message);
+//  const Message = 'Please complete your survey';
+//  console.log(phone_num["1"]);
+//  sendSms(phone_num[1], Message);
 
 
- res.send('Success'); 
- });
+//  res.send('Success'); 
+//  });
 
   app.get('/survey/:id', function(req, res){
       res.render('pages/survey');//,{id: req.params.id}
