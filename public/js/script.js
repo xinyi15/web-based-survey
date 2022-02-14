c7f49aae0d2807290f0fe9182d483d7a50a4f738
Zhang Xinyi <surveyappunc@gmail.com>
2022-02-14 14:52:54 -0500

    using git for heroku deployment

--- public/js/script.js
+++ public/js/script.js
@@ -8,11 +8,12 @@ if (surveyName === "uncTeenSurvey") {
 } else if (surveyName === "uncTeen") {
 }
 
-var name = "{{id}}";
-console.log("DDDD",name);
+
 
 function insertQuestions(questions) {
     let newPages = [];
+    var name = "{{id}}";
+console.log("DDDD",name);
     for (let i = 0; i < questions.pages.length; i++) {
         if (questionList.includes(questions.pages[i].questions[0].name)) {
             newPages.push(questions.pages[i]);
