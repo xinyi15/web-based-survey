0e770b83ab3dd4f4e240697b99c88d6273b0cfd5
Zhang Xinyi <surveyappunc@gmail.com>
2022-03-14 11:51:28 -0400

    first commit

--- index.js
+++ index.js
@@ -47,10 +47,10 @@ const client = await pool.connect();
   let hours = date_ob.getHours();
 
   console.log(date_ob);
-  if(hours>=6){
-    hours=hours-6
+  if(hours>=4){
+    hours=hours-4
    }else{
-     hours=24-(6-hours)
+     hours=24-(4-hours)
    } 
     if((hours>=start)&(hours<end)){
       return false;
