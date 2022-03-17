e71afd024588831b2477307a42a10cb1d9e79782
xinyi7 <xinyi7@ad.unc.edu>
2022-03-17 15:37:16 -0400

    first commit

--- public/js/helper.js
+++ public/js/helper.js
@@ -1,9 +1,9 @@
 import {uncTeenSurvey} from "/js/surveylist.js";
 import {surveyref} from "/js/UniqueSurvey.js";
 export function GetSurvey(surveyName) {
-    console.log(surveyref[surveyName]== "uncTeenSurvey");
+   // console.log(surveyref[surveyName]== "uncTeenSurvey");
  if (surveyref[surveyName] == "uncTeenSurvey") {
-    console.log(surveyref[surveyName]);
+    //console.log(surveyref[surveyName]);
         return(uncTeenSurvey);
     } else if (surveyref[surveyName] === "uncTeen") {
     }
