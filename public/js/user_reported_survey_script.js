4675e51241b7efd85ded4711ec939ad001b58d4c
xinyi7 <xinyi7@ad.unc.edu>
2022-03-18 13:11:57 -0400

    first commit

--- public/js/user_reported_survey_script.js
+++ public/js/user_reported_survey_script.js
@@ -27,7 +27,7 @@ function insertQuestions(questions) {
     let qeustiondescriptions = [];
     let qeustionnames = [];
     for (let i = 0; i < questions.pages.length; i++) {
-        if(![11,12,14,15,16,17,,20,21,22,23].includes(i)){
+        if(![12,15,16,17,21,22,23].includes(i)){
         qeustiondescriptions.push(questions.pages[i].questions[0].title);
         qeustionnames.push(questions.pages[i].questions[0].name);
         }
